24acc1e0fd82082e6fe82a2249b1eaa8e729aea2
Dan Nemeth <dnemeth@pivotal.io>
2016-02-21 20:24:34 -0500

    Created getRepo helper method for integration testing.

--- integrations/_test/mongo_test.go
+++ integrations/_test/mongo_test.go
@@ -1,6 +1,7 @@
 package integrations_test
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"testing"
@@ -10,22 +11,30 @@ import (
 	. "github.com/cloudnativego/gogo-service"
 )
 
+func getRepo(collectionName string) (col cfmgo.Collection, err error) {
+	host := os.Getenv("MONGO_PORT_27017_TCP_ADDR")
+	port := os.Getenv("MONGO_PORT_27017_TCP_PORT")
+
+	if len(host) == 0 {
+		err = errors.New("Could not retrieve mongo host information.")
+		return
+	}
+	if len(port) == 0 {
+		port = "27017"
+	}
+
+	uri := fmt.Sprintf("mongodb://%s:%s/fake-guid", host, port)
+	col = cfmgo.Connect(cfmgo.NewCollectionDialer, uri, collectionName)
+
+	return
+}
+
 func TestAddMatchToRepo(t *testing.T) {
-	mongoHost := os.Getenv("MONGO_PORT_27017_TCP_ADDR")
-	mongoPort := os.Getenv("MONGO_PORT_27017_TCP_PORT")
-
-	if len(mongoHost) == 0 {
-		t.Fatal("Could not retrieve mongo host information")
+	matches, err := getRepo("matches")
+	if err != nil {
+		t.Fatalf("Error: %v", err)
 	}
-	if len(mongoPort) == 0 {
-		mongoPort = "27017"
-	}
-
-	mongoURI := fmt.Sprintf("mongodb://%s:%s/fake-guid", mongoHost, mongoPort)
-	fmt.Println("MONGOURI:\t" + mongoURI)
-	matchesCollection := cfmgo.Connect(cfmgo.NewCollectionDialer, mongoURI, "matches")
-
-	repo := NewMongoMatchRepository(matchesCollection)
+	repo := NewMongoMatchRepository(matches)
 	if repo == nil {
 		t.Error("Error creating Mongo repo")
 	}
